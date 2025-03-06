c74b6e65d261ea7660062b8c9cbed5a63ffd7339
akshath7135 <akshathabrol21122004@gmail.com>
2025-03-06 12:05:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function validateForm() {
+	const age = document.getElementById('age').value;
+	const name = document.getElementById('name').value;
+	if(age === '' || name === ''){
+		alert{"Please enter valid details."}
+		return false;
+	}
+	const ageNumber = parseInt(age);
+	return new Promise((resolve,reject)=>{
+		setTimeout(() => {
+          if (ageNumber > 18) {
+            resolve(`Welcome, ${name}. You can vote.`);
+          } else {
+            reject(`Oh sorry ${name}. You aren't old enough.`);
+          }
+        }, 4000);	
+		}).then((message)=>{
+		alert(message);
+		}).catch((error)=>{
+		alert(error);
+		});
+	return false;
+}
